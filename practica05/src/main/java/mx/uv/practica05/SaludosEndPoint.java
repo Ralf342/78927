1457fedde3e35f8d887cccc052ce6bac69f16313
Ralf342 <j.oskar.v282143@gmail.com>
2024-04-29 12:40:46 -0600

    practica05

---
+++ practica05/src/main/java/mx/uv/practica05/SaludosEndPoint.java
@@ -0,0 +1,21 @@
+package mx.uv.practica05;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import mx.uv.t4is.saludos.SaludarRequest;
+import mx.uv.t4is.saludos.SaludarResponse;
+
+@Endpoint
+public class SaludosEndPoint {
+    @PayloadRoot(localPart = "SaludarRequest",namespace = "t4is.uv.mx/saludos")
+
+    @ResponsePayload
+    public SaludarResponse saludar(@RequestPayload SaludarRequest nombre){
+        SaludarResponse respuesta =new SaludarResponse();
+        respuesta.setRespuesta("hola " + nombre.getNombre());
+        return respuesta;
+    }
+}
