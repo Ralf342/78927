ca76b5f8ddd3c4cb6fa8f43580c2ee1ae903f342
Ralf342 <j.oskar.v282143@gmail.com>
2024-04-30 12:31:06 -0600

    Practica05

--- practica05/src/main/java/mx/uv/practica05/SaludosEndPoint.java
+++ practica05/src/main/java/mx/uv/practica05/SaludosEndPoint.java
@@ -5,17 +5,43 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import mx.uv.t4is.saludos.BuscarSaludosResponse;
 import mx.uv.t4is.saludos.SaludarRequest;
 import mx.uv.t4is.saludos.SaludarResponse;
 
 @Endpoint
 public class SaludosEndPoint {
-    @PayloadRoot(localPart = "SaludarRequest",namespace = "t4is.uv.mx/saludos")
+
+
+    List<Saludos> saludos = new ArrayList<>();
+
+    @PayloadRoot(localPart = "SaludarRequest", namespace = "t4is.uv.mx/saludos")
 
     @ResponsePayload
-    public SaludarResponse saludar(@RequestPayload SaludarRequest nombre){
-        SaludarResponse respuesta =new SaludarResponse();
-        respuesta.setRespuesta("hola " + nombre.getNombre());
+    public SaludarResponse saludar(@RequestPayload SaludarRequest peticion){
+        SaludarResponse respuesta = new SaludarResponse();
+        respuesta.setRespuesta("hola" + peticion.getNombre());
+        /*Simular persistencia */
+        //saludos.add(peticion.getNombre());
+        Saludos e = new Saludos();
+        e.setId(0);
+        e.setNombre(peticion.getNombre());
+        saludos.add(e);
         return respuesta;
     }
+
+    @PayloadRoot(localPart = "BuscarSaludosRequest", namespace = "t4is.uv.mx/saludos")
+    @ResponsePayload
+    public BuscarSaludosResponse buscarSaludos(){
+
+        BuscarSaludosResponse respuesta = new BuscarSaludosResponse();
+        //respuesta.getSaludos().add();
+        
+        for (int i = 0; i < saludos.size(); i++){
+            System.out.println(saludos.get(i));
+            respuesta.getSaludos().add(saludos.get(i));
+        }
+        return respuesta;
+    }
+    
 }
